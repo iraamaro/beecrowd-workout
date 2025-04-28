b3d7cae3c952a3d010dcedfedbb2052c9dd22a54
Ivan A <iramaro@disroot.org>
2025-04-27 23:17:29 -0300

    Finisehd 1002 problem.

---
+++ lib/1002.rb
@@ -0,0 +1,25 @@
+class Area
+  def self.call
+    r = get_number("Digite o raio da circunferência: ")
+    # a = Math::PI * r**2
+    a = 3.14159 * r**2
+    puts format('A=%.4f', a)
+  end
+
+  def self.get_number(mensagem)
+    loop do
+      print mensagem
+      input = gets.chomp
+      if valid_number?(input)
+        number = input.to_f
+        return number if number > 0
+      else
+        puts "Entrada inválida. Por favor, digite um número positivo."
+      end
+    end
+  end
+
+  def self.valid_number?(input)
+    input.match?(/\A\d+(\.\d+)?\z/)
+  end
+end
---
+++ spec/1002_spec.rb
@@ -0,0 +1,94 @@
+require 'spec_helper'
+require '1002.rb'
+
+describe Area do
+  before(:each) do
+    @area = Area.new
+  end
+  describe 'Uso do método .call' do
+    context 'quando chamo .call com raio 2' do
+      it 'deve retornar A=12.5664' do
+        allow_any_instance_of(Object).to receive(:gets).and_return("2")
+        expect { described_class.call }.to output(
+          "Digite o raio da circunferência: A=12.5664\n"
+        ).to_stdout
+      end
+    end
+    context 'quando chamo .call com raio 100.64' do
+      it 'deve retornar A=31819.3103' do
+        allow_any_instance_of(Object).to receive(:gets).and_return("100.64")
+        expect { described_class.call }.to output(
+          "Digite o raio da circunferência: A=31819.3103\n"
+        ).to_stdout
+      end
+    end
+    context 'quando chamo .call com raio 150' do
+      it 'deve retornar A=70685.7750' do
+        allow_any_instance_of(Object).to receive(:gets).and_return("150")
+        expect { described_class.call }.to output(
+          "Digite o raio da circunferência: A=70685.7750\n"
+        ).to_stdout
+      end
+    end
+    context 'quando chamo .call com raio negativo e depois correto com 2' do
+      it 'pede novamente se o usuário digitar número negativo' do
+        allow_any_instance_of(Object).to receive(:gets).and_return("-2", "2")
+        expect { described_class.call }.to output(
+          "Digite o raio da circunferência: Entrada inválida. " +
+          "Por favor, digite um número positivo.\n" \
+          "Digite o raio da circunferência: A=12.5664\n"
+        ).to_stdout
+      end
+    end
+    context 'quando chamo .call com caractére inválido e depois correto com 2' do
+      it 'pede novamente se o usuário digitar um caractére inválido' do
+        allow_any_instance_of(Object).to receive(:gets).and_return("xxx", "2")
+        expect { described_class.call }.to output(
+          "Digite o raio da circunferência: Entrada inválida. " +
+          "Por favor, digite um número positivo.\n" \
+          "Digite o raio da circunferência: A=12.5664\n"
+        ).to_stdout
+      end
+    end
+    context 'quando chamo .call com raio negativo e depois correto com 100.64' do
+      it 'pede novamente se o usuário digitar número negativo' do
+        allow_any_instance_of(Object).to receive(:gets).and_return("-100.64", "100.64")
+        expect { described_class.call }.to output(
+          "Digite o raio da circunferência: Entrada inválida. " +
+          "Por favor, digite um número positivo.\n" \
+          "Digite o raio da circunferência: A=31819.3103\n"
+        ).to_stdout
+      end
+    end
+    context 'quando chamo .call com caractére inválido e depois correto com 100.64' do
+      it 'pede novamente se o usuário digitar um caractére inválido' do
+        allow_any_instance_of(Object).to receive(:gets).and_return("@_=p[s", "100.64")
+        expect { described_class.call }.to output(
+          "Digite o raio da circunferência: Entrada inválida. " +
+          "Por favor, digite um número positivo.\n" \
+          "Digite o raio da circunferência: A=31819.3103\n"
+        ).to_stdout
+      end
+    end
+    context 'quando chamo .call com raio negativo e depois correto com 150' do
+      it 'pede novamente se o usuário digitar número negativo' do
+        allow_any_instance_of(Object).to receive(:gets).and_return("-150", "150")
+        expect { described_class.call }.to output(
+          "Digite o raio da circunferência: Entrada inválida. " +
+          "Por favor, digite um número positivo.\n" \
+          "Digite o raio da circunferência: A=70685.7750\n"
+        ).to_stdout
+      end
+    end
+    context 'quando chamo .call com caractére inválido e depois correto com 150' do
+      it 'pede novamente se o usuário digitar um caractére inválido' do
+        allow_any_instance_of(Object).to receive(:gets).and_return("0=-^~><kas", "150")
+        expect { described_class.call }.to output(
+          "Digite o raio da circunferência: Entrada inválida. " +
+          "Por favor, digite um número positivo.\n" \
+          "Digite o raio da circunferência: A=70685.7750\n"
+        ).to_stdout
+      end
+    end
+  end
+end
